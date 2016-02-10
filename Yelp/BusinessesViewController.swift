ef78a03c72dcd9cc837aa2ba40400dadfc658e53
Andrew <yu.drew@gmail.com>
2016-02-09 19:18:01 -0800

    Link data from yelp API call to cells

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -3,21 +3,28 @@
 //  Yelp
 //
 //  Created by Timothy Lee on 4/23/15.
+//  Developed by Andrew Yu starting 2/9/16
 //  Copyright (c) 2015 Timothy Lee. All rights reserved.
 //
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
 
     var businesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
 
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
-        
+            self.tableView.reloadData()
             for business in businesses {
                 print(business.name!)
                 print(business.address!)
@@ -34,6 +41,22 @@ class BusinessesViewController: UIViewController {
             }
         }
 */
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        }
+        else {
+            return 0
+        }
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+     
+        cell.business = businesses[indexPath.row]
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
